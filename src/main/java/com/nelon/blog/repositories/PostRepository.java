34ab0d9525f4b6b60cbe2f25bd6f38e72da979b1
NelonArrive <nelon.arrive@gmail.com>
2025-11-09 18:25:06 +0500

    feat: add repository interfaces for Category, Post, Tag, and User

---
+++ src/main/java/com/nelon/blog/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.nelon.blog.repositories;
+
+import com.nelon.blog.domain.dto.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+}
---
+++ src/main/java/com/nelon/blog/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.nelon.blog.repositories;
+
+import com.nelon.blog.domain.dto.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
